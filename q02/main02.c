6e530894c30edcfe14cdf6c99044c514443c4b44
Igor Trentini <igortrentini.2004@gmail.com>
2022-05-05 21:02:16 -0300

    feat: Finalizado segunda questao

--- q02/main02.c
+++ q02/main02.c
@@ -3,20 +3,41 @@
 #include <ctype.h>
 
 int main(void) {
-    char response = '';
+    char response;
+    float temperature = 0.0;
 
-    printf("O paciente está saudável: ");
-    scanf(" %c%*[^\n]", &response);
     system("clear||cls");
 
+    printf("O paciente está saudável?\n-> [S] Sim\n-> [Outro caractere] Não\n\n");
+    scanf(" %c%*[^\n]", &response);
+    system("clear||cls");
     response = toupper(response);
 
     if (response == 'S') {
-        printf("O paciente está saudável.")
+        printf("O paciente está saudável.");
         return 0;
     }
 
-    
+    printf("O paciente tem dor?\n-> [S] Sim\n-> [Outro caractere] Não\n\n");
+    scanf(" %c%*[^\n]", &response);
+    system("clear||cls");
+    response = toupper(response);
+
+    if (response == 'S') {
+        printf("O paciente está doente.");
+        return 0;
+    }
+
+    printf("Qual a temperatura do paciente?\n\n");
+    scanf("%f", &temperature);
+    system("clear||cls");
+
+    if (temperature <= (float)37) {
+        printf("O paciente está saudável.");
+        return 0;
+    }
+
+    printf("O paciente está doente.");
 
     return 0;
 }
